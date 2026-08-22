efe4d0ac48d274f48aa39cf7975e0510f3a6ba01
niyazasghar <niyazasghar12@gmail.com>
2026-08-23 00:58:34 +0530

    Enhance mobile responsiveness and correct text address

--- src/app/join-us/internship/page.tsx
+++ src/app/join-us/internship/page.tsx
@@ -143,7 +143,7 @@ export default function InternshipPage() {
                     </FormItem>
                   )}
                 />
-                <div className="grid grid-cols-2 gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   <FormField
                     control={form.control}
                     name="duration"
